070755a9acf85a6a28bfa3e1ee56fb059016c104
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2024-02-27 16:10:40 +0700

    Initial commit

---
+++ scripts/api.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+import gradio as gr
+from fastapi import FastAPI
+from fastapi import status
+from fastapi.responses import JSONResponse
+from modules.api.models import *
+from modules.api import api
+
+
+GRADIO_CACHE_DIR = os.getenv('GRADIO_CLEANER_DIR', '/temp/gradio')
+
+
+def sd_gradio_cleaner_api(_: gr.Blocks, app: FastAPI):
+    @app.post("/clean_cache")
+    async def clean_cache():
+        try:
+            shutil.rmtree(GRADIO_CACHE_DIR)
+        except Exception as e:
+            return JSONResponse(
+                content=str(e),
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        return JSONResponse(
+            content="Ok",
+            status=status.HTTP_200_OK,
+        )
+
+try:
+    import modules.script_callbacks as script_callbacks
+
+    script_callbacks.on_app_started(sd_gradio_cleaner_api)
+except:
+    pass
